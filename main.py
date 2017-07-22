25c5eb80ebf4ebe24e5594eb84ca1ac7345b1fcc
NCPlayz <NCPlayz@users.noreply.github.com>
2017-07-22 22:39:49 +0100

    Listen To Albums and Summon
    Update #05Cass
    - Added 'are you ready to begin' as another summoning command.
    - Fixed issue where you could write 'cassandra are you ready to begin' anywhere in the sentence.
    - Added Music Album Support for All The Lights In The Sky and Modern Synthesis.
    - Now uses youtube-dl
    --Coming Up In A Few Days--
    ~ Music Album Support for Blackline, Underline, and Let It Resonate.
    ~ Command to be able to stop the music.

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     # Ping warning
     # change last 2 conditionals to single if have mod role but cba atm
     if ("301392743840874497" in message.content) and message.author.id != client.user.id and message.author.id != "227187657715875841" and message.author.id != "108875988967882752":
@@ -36,10 +35,10 @@ async def on_message(message):
         log(logMsg)
 
     # System;Start #1
-    if message.content.lower().startswith("cassandra can you hear me"):
+    if message.content.lower() == "cassandra can you hear me":
         if message.author.voice.voice_channel == None:
             if True:
-                await client.send_message(message.channel, "Yes.")
+                await bot.send_message(message.channel, "Yes.")
                 logMsg = message.author.name + " asked Cassandra if she could hear them (text)"
                 log(logMsg)
         else:
@@ -52,12 +51,12 @@ async def on_message(message):
             logMsg = message.author.name + " asked Cassandra if she could hear them (voice)"
             log(logMsg)
     # System;Start #2
-    if message.content.lower().startswith("cassandra are you ready to begin"):
+    if message.content.lower() == "cassandra are you ready to begin" or message.content.lower() == "are you ready to begin":
         if message.author.voice.voice_channel == None:
             if True:
-                await client.send_message(message.channel, "Yes,")
+                await bot.send_message(message.channel, "Yes,")
                 time.sleep(1)
-                await client.send_message(message.channel, "I'm ready.")
+                await bot.send_message(message.channel, "I'm ready.")
                 logMsg = message.author.name + " asked Cassandra if she was ready to begin (text)"
                 log(logMsg)
         else:
@@ -80,64 +79,112 @@ async def role(ctx, action : str, role : str):
     acceptableTypes = ["add", "remove", "+", "-"]
 
     if(action in acceptableTypes and role in acceptableRoles):
-        if action == "add" or action == "+":
+        if action is "add" or action is "+":
             try:
                 await bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name=role))
             except:
-                await bot.send_message(ctx.message.channel, "Failed to add `" + role + "` role to " + ctx.message.author.name)
-                logMsg = message.author.name + " failed to add the " + role + " role to themselves"
-                print(logMsg)
+                await say(contextChannel, "Failed to add `" + role + "` role to " + ctx.message.author.name)
+                logMsg = ctx.message.author.name + " failed to add the " + role + " role to themselves"
+                log(logMsg)
             finally:
                 await bot.send_message(ctx.message.channel, "Successfully added `" + role + " ` role to " + ctx.message.author.name)
                 logMsg = ctx.message.author.name + " added the " + role + " role to themselves"
-                print(logMsg)
+                log(logMsg)
         else:
             try:
                 await bot.remove_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name=role))
             except:
                 await bot.send_message(ctx.message.channel, "Failed to remove `" + role + "` role from " + ctx.message.author.name)
                 logMsg = ctx.message.author.name + " failed to remove the " + role + " role from themselves"
-                print(logMsg)
+                log(logMsg)
             finally:
                 await bot.send_message(ctx.message.channel, "Successfully removed `" + role + " ` role from " + ctx.message.author.name)
                 logMsg = ctx.message.author.name + " removed the " + role + " role from themselves"
-                print(logMsg)
+                log(logMsg)
     elif action not in acceptableTypes:
         await bot.send_message(ctx.message.channel, "Invalid parameter!")
     elif action in acceptableTypes and role not in acceptableRoles:
         await bot.send_message(ctx.message.channel, "Invalid role!")
-'''@bot.command(pass_context = True)
+@bot.command(pass_context = True)
 async def playlist(ctx, playlist : str):
-    if playlist == None:
+    #youtube_dl.utils.DownloadError: ERROR: Unable to download webpage: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:645)> (caused by URLError(SSLError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:645)'),))
+    #Are you getting the above error? https://github.com/rg3/youtube-dl/issues/11573#issuecomment-269896421
+    playlistAlbums = ['ATLITS', 'MS', 'Blackline', 'Underline', 'LIR']
+    if playlist not in playlistAlbums:
         bot.send_message(ctx.message.channel, 'You have not chosen a playlist. The playlists are: ATLITS, MS, Blackline, Underline, LIR')
-    else:
-        if ctx.message.author.voice.voice_channel == None:
+    elif(playlist in playlistAlbums):
+        if ctx.message.author.voice.voice_channel is None:
             await bot.send_message(ctx.message.channel, 'You are not in a voice channel.')
         else:
-            vc = discord.utils.get(message.server.channels, id=message.author.voice.voice_channel.id)
-            voice = await client.join_voice_channel(vc)
-            player = voice.create_ffmpeg_player('ss2.mp3')
-            player.start()
-            time.sleep(5)
-            await voice.disconnect()
-            logMsg = message.author.name + " asked Cassandra if she was ready to begin (voice)"
-            log(logMsg)
-@bot.command(pass_context = True)
+            await bot.send_message(ctx.message.channel, 'Please Wait...')
+            if (playlist == "ATLITS"):
+                thePlaylist = "https://www.youtube.com/watch?v=KyUYbAWjdx8"
+                nowPlaying = "All The Lights In The Sky"
+                bot.send_message(ctx.message.channel, 'Now playing the ' + nowPlaying + ' album by Area 11.')
+                vc = ctx.message.author.voice.voice_channel
+                voice = await client.join_voice_channel(vc)
+                player = await voice.create_ytdl_player(thePlaylist)
+                player.start()
+                logMsg = ctx.message.author.name + " started the " + nowPlaying + " playlist in " + ctx.message.author.voice.voice_channel.name + " Voice Channel."
+                log(logMsg)
+                #await bot.send_message(ctx.message.channel, "**[WIP]** As of this update, an official playlist of all of the songs in **All The Lights In The Sky** has not been made. Hopefully by the next update, there will be a playlist of all of the songs in **All The Lights In The Sky**. " + ctx.message.author.mention)
+            elif (playlist == "MS"):
+                thePlaylist = "https://www.youtube.com/watch?v=mmiCnhC0Q-o&ab_channel=ViperFinn"
+                nowPlaying = "Modern Synthesis"
+                bot.send_message(ctx.message.channel, 'Now playing the ' + nowPlaying + ' album by Area 11.')
+                vc = ctx.message.author.voice.voice_channel
+                voice = await client.join_voice_channel(vc)
+                player = await voice.create_ytdl_player(thePlaylist)
+                player.start()
+                logMsg = ctx.message.author.name + " started the " + nowPlaying + " playlist in " + ctx.message.author.voice.voice_channel.name + " Voice Channel." 
+                log(logMsg)
+            elif (playlist == "Blackline"):
+                '''thePlaylist = "NULL"
+                vc = ctx.message.author.voice.voice_channel
+                voice = await client.join_voice_channel(vc)
+                player = await voice.create_ytdl_player(thePlaylist)
+                logMsg = ctx.message.author.name + " started the " + playlist + " playlist in " + ctx.message.author.voice.voice_channel.name + " Voice Channel." 
+                log(logMsg)
+                player.start()
+                nowPlaying = "Blackline"'''
+                await bot.send_message(ctx.message.channel, "**[WIP]** As of this update, an official playlist of all of the songs in **Blackline EP** has not been made. Hopefully by the next update, there will be a playlist of all of the songs in **Blackline EP**. " + ctx.message.author.mention)
+            elif (playlist == "Underline"):
+                '''thePlaylist = "NULL"
+                vc = ctx.message.author.voice.voice_channel
+                voice = await client.join_voice_channel(vc)
+                player = await voice.create_ytdl_player(thePlaylist)
+                logMsg = ctx.message.author.name + " started the " + playlist + " playlist in " + ctx.message.author.voice.voice_channel.name + " Voice Channel." 
+                log(logMsg)
+                player.start()
+                nowPlaying = "Underline"'''
+                await bot.send_message(ctx.message.channel, "**[WIP]** As of this update, an official playlist of all of the songs in **Underline** has not been made. Hopefully by the next update, there will be a playlist of all of the songs in **Underline**. " + ctx.message.author.mention)
+            elif (playlist == "LIR"):
+                '''thePlaylist = "NULL"
+                vc = ctx.message.author.voice.voice_channel
+                voice = await client.join_voice_channel(vc)
+                player = await voice.create_ytdl_player(thePlaylist)
+                logMsg = ctx.message.author.name + " started the " + playlist + " playlist in " + ctx.message.author.voice.voice_channel.name + " Voice Channel." 
+                log(logMsg)
+                player.start()
+                nowPlaying = "Let It Resonate"'''
+                await bot.send_message(ctx.message.channel, "**[WIP]** As of this update, an official playlist of all of the songs in **Let It Resonate** has not been made. Hopefully by the next update, there will be a playlist of all of the songs in **Let It Resonate**. " + ctx.message.author.mention)
+#NOT WORKING ATM
+'''@bot.command(pass_context = True)
 async def stop_voice(ctx):
-    if bot.voice.voice_channel == None:
+    if client.is_voice_connected(ctx.message.server) is False:
         await bot.send_message(ctx.message.channel, 'I am not in a voice channel.')
     else:
         await bot.send_message(ctx.message.channel, 'Stopping...')
-        await voice.disconnect()
-        await bot.send_message(ctx.message.channel, 'Left' + voice_channel.name)'''
+        await client.voice.disconnect()
+        await bot.send_message(ctx.message.channel, 'L' + stopVoiceChannelName)'''
     #About Command
 @bot.command(pass_context = True)
 async def about(ctx):
     """Tells you about this bot."""
     aboutEmbed = discord.Embed(title='About Cassandra', description="Custom Discord Bot", url="https://github.com/Avinch/CassBotPy", color=discord.Color.gold())
     aboutEmbed.set_footer(text=version)
-    aboutEmbed.set_thumbnail(url=bot.user.avatar_url) #aboutEmbed.set_thumbnail(url=client.user.avatar_url)
-    await bot.send_message(ctx.message.channel, embed=aboutEmbed) #await client.send_message(message.channel, embed=aboutEmbed)
+    aboutEmbed.set_thumbnail(url=bot.user.avatar_url)
+    await bot.send_message(ctx.message.channel, embed=aboutEmbed)
 
 def log(message):
     print(datetime.datetime.now(), message)
